5e8ffc4198207401a18bfb66aeb7f2619c487942
majowyporanek <klaudiachwistekklaudia@gmail.com>
2023-11-14 01:12:12 +0100

    clean-up

--- Zadanie4/4.7.py
+++ Zadanie4/4.7.py
@@ -1,3 +1,11 @@
+# ZADANIE 4.7 Mamy daną sekwencję, w której niektóre z elementów mogą okazać się podsekwencjami,
+# a takie zagnieżdżenia mogą się nakładać do nieograniczonej głębokości. Napisać funkcję flatten(sequence),
+# która zwróci spłaszczoną listę wszystkich elementów sekwencji. Wskazówka: rozważyć wersję rekurencyjną,
+# a sprawdzanie czy element jest sekwencją, wykonać przez isinstance(item, (list, tuple)).
+#
+# seq = [1,(2,3),[],[4,(5,6,7)],8,[9]]
+# print(flatten(seq))   # [1,2,3,4,5,6,7,8,9]
+
 def flatten(sekwencja):
     lista = []
     for x in sekwencja:
